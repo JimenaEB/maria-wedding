acb2b0b76d70b0c04006740dd083ed94dc5dc4b1
Jimena Escobar Bermúdez <jime.ebermudez@gmail.com>
2021-09-06 21:51:36 +0000

    unfinished changes

--- src/js/component/history.jsx
+++ src/js/component/history.jsx
@@ -1,7 +1,21 @@
 import React from "react";
+import PropTypes from "prop-types";
+
+import Polaroid from "./polaroid.jsx";
 
 const History = props => {
-	return <section></section>;
+	return (
+		<section>
+			<p>{props.text[0]}</p>
+			<p>{props.text[1]}</p>
+			<Polaroid img={props.img} />
+		</section>
+	);
+};
+
+History.propTypes = {
+	text: PropTypes.array.isRequired,
+	img: PropTypes.array.isRequired
 };
 
 export default History;
