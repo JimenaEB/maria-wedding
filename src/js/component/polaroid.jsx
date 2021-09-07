f7c07de322f99765cc8dba3227019679bb3dcb57
Andrés Orcajo <{ID}+{username}@users.noreply.github.com>
2021-09-07 02:13:06 +0200

    Now the polaroid is the best component in da web 🥸

--- src/js/component/polaroid.jsx
+++ src/js/component/polaroid.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
+const captions = ["😍", "😘", "🥰", "🤪", "🤩", "❤️"];
+
 const Polaroid = props => {
+	const [imageIndex, setImageIndex] = useState(0);
+	const [currentImg, setCurrentImg] = useState(props.img[imageIndex]);
+	const [imgCaption, setImgCaption] = useState(captions[0]);
+
+	useEffect(() => {
+		const timer = setTimeout(() => {
+			setImageIndex(imageIndex + 1);
+			setCurrentImg(props.img[imageIndex % props.img.length]);
+			setImgCaption(
+				captions[Math.floor(Math.random() * captions.length)]
+			);
+		}, 4500);
+	});
+
 	return (
-		<figure clallName="polaroid">
-			<img src={props.img[0]} alt="" />
+		<figure className="polaroid">
+			<img src={currentImg} alt="" />
+			<figcaption>{imgCaption}</figcaption>
 		</figure>
 	);
 };
