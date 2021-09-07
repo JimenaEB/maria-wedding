2533ac9d1f7dc2f7523c7b7d069e86c3af224aaa
Jimena Escobar Bermúdez <jime.ebermudez@gmail.com>
2021-09-07 10:36:59 +0000

    minor fix

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -46,20 +46,6 @@ const Home = () => {
 			<History img={images} text={text} />
 			<GForm></GForm>
 		</Fragment>
-		// <div className="text-center mt-5">
-		// 	<h1>Hello Rigo!</h1>
-		// 	<p>
-		// 		<img src={mariaDaniPiscina} />
-		// 	</p>
-		// 	<a href="#" className="btn btn-success">
-		// 		If you see this green button... bootstrap is working
-		// 	</a>
-		// 	<p>
-		// 		Made by{" "}
-		// 		<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-		// 		love!
-		// 	</p>
-		// </div>
 	);
 };
 
