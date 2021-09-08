e5794d5725e42369469448892a590bd8cc0c9c39
Jimena Escobar Bermúdez <jime.ebermudez@gmail.com>
2021-09-08 23:54:35 +0000

    ceremony

--- src/js/component/ceremony.jsx
+++ src/js/component/ceremony.jsx
@@ -1,31 +1,80 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const Ceremony = props => {
+	const [text, setText] = useState("");
+
+	useEffect(() => {
+		if (props.disposition == "right") {
+			setText(
+				<article className="ceremony_text">
+					<p>
+						La ceremonia será a las
+						<span className="bold"> 11:45 </span>
+						en la capilla del colegio en dónde se conocieron.
+					</p>
+					<p>
+						El colegio H.H. Maristas de Alicante se encuentra en la
+						<span className="bold"> C/ Isla de Corfú, 5</span>.
+					</p>
+					<p>
+						Es fácil aparcar en la zona en caso de asistir en
+						vehículo propio.
+					</p>
+					<a href={props.text}>
+						<button className="ceremony_button">
+							Ver en el mapa
+						</button>
+					</a>
+					<p>
+						Se ruega puntualidad ya que
+						<span className="bold"> la novia </span>llegará a las
+						12:00 y{" "}
+						<span className="bold">
+							{" "}
+							debe ser la última en llegar
+						</span>
+						.
+					</p>
+				</article>
+			);
+		} else {
+			setText(
+				<article className="ceremony_text">
+					<p>
+						El banquete y fiesta serán en la
+						<span className="bold"> Finca Torre Bosch </span>
+					</p>
+					<p>
+						La finca se encuentra en la
+						<span className="bold">
+							{" "}
+							Avenida Conrado Albadalejo, 3
+						</span>
+						.
+					</p>
+					<a href={props.text}>
+						<button className="ceremony_button">
+							Ver en el mapa
+						</button>
+					</a>
+					<p>
+						El cocktail comenzará a las
+						<span className="bold"> 14:00 </span>y la comida a las
+						<span className="bold"> 15:00</span>.
+					</p>
+					<p>
+						Para vuestra tranquilidad dispondremos de autobúses
+						tanto de ida cómo de vuelta al centro de Alicante.
+					</p>
+				</article>
+			);
+		}
+	}, []);
+
 	return (
 		<section className={"ceremony ".concat(props.disposition)}>
-			<article>
-				<p>
-					La ceremonia será a las
-					<span className="bold">11:45</span>
-					en la capilla del colegio en dónde se conocieron.
-				</p>
-				<p>
-					El colegio H.H. Maristas de Alicante se encuentra en la
-					<span className="bold"> C/ Isla de Corfú, 5</span>.
-				</p>
-				<p>
-					Es fácil aparcar en la zona en caso de asistir en vehículo
-					propio.
-				</p>
-				<button>
-					<a href={props.text}>Ver en el mapa</a>
-				</button>
-				<p>
-					Se ruega puntualidad ya que la novia llegará a las 12:00 y
-					debe ser la última en llegar.
-				</p>
-			</article>
+			{text}
 			<img src={props.img} alt="foto del sitio" />
 		</section>
 	);
